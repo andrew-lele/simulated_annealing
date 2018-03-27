e67851c5e9fefce8a170dd33afba691ea2a41821
andrew-lele <andrew.le197@gmail.com>
2018-03-27 00:42:35 -0400

    initial commit from brute force

--- local_search.py
+++ local_search.py
@@ -6,41 +6,65 @@ import numpy as np
 from ad import adnumber
 
 #LOCAL SEARCH
-def hill_Climb(code, xmin, ymin, xmax, ymax):
+def hill_Climb(fxn, xmin, ymin, xmax, ymax):
 	print("... execute hill_Climb ... ")
-	
-	#get intial values - num_restarts is constant
+	print("fxn: " ,fxn)
+	# get intial values - num_restarts is constant, using derivatives
 	NUM_RESTARTS = 1000
-	x_theta = adnumber( randint(xmin, xmax) )
-	y_theta = adnumber( randint(ymin, ymax) )
-	x_theta,y_theta = my_func(x_theta.x_theta, y_theta.y_theta)
-	ALPHA = 1
+
+	x = randint(xmin, xmax) 
+	y = randint(ymin, ymax) 
+	# x_theta = adnumber( randint(xmin, xmax) )
+	# y_theta = adnumber( randint(ymin, ymax) )
+	theta = my_func(x, y)
+	theta_new = theta + 1
+	ALPHA = 2
 
 	for i in range(0, NUM_RESTARTS):
-		theta_x = theta_x - ALPHA
-		theta_y = theta_y - ALPHA
+		thetas = [my_func(x + ALPHA, y), my_func(x, y + ALPHA), my_func(x - ALPHA, y),my_func(x, y - ALPHA)  ]
+		direction = max(thetas)
+		# theta1 = my_func(x + ALPHA, y) 
+		# theta2 = my_func(x, y + ALPHA) 
+		# theta3 = my_func(x - ALPHA, y) 
+		# theta4 = my_func(x, y - ALPHA) 
+
+		# theta_new = max(theta4, theta3, theta2, theta1, theta)
+		# while theta != theta_new:
+		# 	theta_x = theta_x - ALPHA*
+		# 	theta_y = theta_y - ALPHA
+
+	#ugly brute force method
+	# NUM_RESTARTS = 1000
+
+	# for i in range(0, NUM_RESTARTS):
+	# 	x = randint(xmin, xmax) 
+	# 	y = randint(ymin, ymax) 
+	# 	theta = my_func(x, y)
+	# 	ALPHA = 1
+	# 	theta_new = theta + 1
+	# 	print("---begin ascent---")
+	# 	while theta_new != theta:
+	# 		theta1 = my_func(x + ALPHA, y) 
+	# 		theta2 = my_func(x, y + ALPHA) 
+	# 		theta3 = my_func(x - ALPHA, y) 
+	# 		theta4 = my_func(x, y - ALPHA) 
+
+	# 		theta_new = max(theta4, theta3, theta2, theta1, theta)
+
+
 
 
 #read in input_file and execute function
-def main(args):
+def main(args, code):
 
 	#initalize given parameters
-	# i_file_name = str(sys.argv[1])
 	xmin = int(sys.argv[2])
 	ymin = int(sys.argv[3])
 	xmax = int(sys.argv[4])
 	ymax = int(sys.argv[5])
-
-	# i_file_name = str(i_file_name)
-	code = ''
-	# with open(i_file_name, 'r') as i_file:
-	# 	code = i_file.read()
-	# exec(code)
-	print(code)
-	
-	my_func(1,2)
 	print("===============================")
-	result = hill_Climb(code, xmin, ymin, xmax, ymax)
+	code = code.split('\n')
+	result = hill_Climb(code[1][8:], xmin, ymin, xmax, ymax)
 
 
 
@@ -56,4 +80,4 @@ if __name__ == '__main__':
 		code = i_file.read()
 	exec(code)
 
-	main(sys.argv[1:])
+	main(sys.argv[1:], code)
